89f34f4ecb5e58cd1a7baff2fbed43bcd83c8260
nichlor <70553339+nichlor@users.noreply.github.com>
2021-06-21 13:44:52 -0400

    Update emote_bomb.sh

--- emote_bomb.sh
+++ emote_bomb.sh
@@ -13,4 +13,3 @@ done
 # egg_id.txt contains egg id, 1 per line
 # key.txt contains KSM acct private keys, 1 per line
 # you must import and fund each wallet manually on polkadot.js.org/apps
-# index.js script from https://github.com/martinloesethjensen/rmrk-emote-batch
