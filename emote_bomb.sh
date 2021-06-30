72b07fee8db22eaf6bdfd9885a8502530e4ed859
nichlor <70553339+nichlor@users.noreply.github.com>
2021-06-30 18:13:47 -0400

    Update emote_bomb.sh
    updated from shell loop based script, to newer

--- emote_bomb.sh
+++ emote_bomb.sh
@@ -1,16 +1,45 @@
 #!/bin/bash
 
-for I in `cat egg_id.txt`; do
-    for J in `cat keys.txt`; do
+
+## SEED_FILE contains keys for accounts used to emote.
+## Multiple TARGET_ID can be set - 1 per line
+## BOMB_LOG sets log file
+
+SEED_FILE='keys.txt'
+RMRK_ID_FILE=rmrk_ids.txt
+TARGET_ID='6802639-24d573f4dfa1d7fd33-KAN-KANL-0000000000006501' 
+BOMB_LOG=bomb.log
+EMOTES='🪓 🏰 🕸 💀 🕷'
+
+
+##  Declare Function
+
+function emote {
+    echo $TARGET_ID > $RMRK_ID_FILE
     node index.js \
-    --id $I \
-    -e ws://kusama-rpc.polkadot.io \
-    -s $J \
-    --emotes 🐣 ; done \
-done
+    --id $RMRK_ID_FILE \
+    -e wss://kusama-rpc.polkadot.io \
+    -s $SEED_FILE \
+    --emotes $EMOTES \
+    --chunk true 2>&1 | tee -a $BOMB_LOG
+}
 
+## emote will run function
 
-######### NOTES #########
-# egg_id.txt contains egg id, 1 per line
-# keys.txt contains KSM acct private keys, 1 per line
-# you must import and fund each wallet manually on polkadot.js.org/apps
+emote && echo "Completed $EMOTES on $TARGET_ID"
+
+## change variables to more easily edit key files and emotes. 
+## repeat as necessary
+## Uncomment to enable additional targets
+
+#TARGET_ID='6802639-24d573f4dfa1d7fd33-KAN-KANL-0000000000006503'
+#EMOTES='⚰️ 🧟‍♀️ 🧛‍♀️'
+#emote && echo "Completed $EMOTES on $TARGET_ID"
+
+#TARGET_ID='6802639-24d573f4dfa1d7fd33-KAN-KANL-0000000000006502'
+#EMOTES='🩸 🦋 🦚'
+#emote && echo "Completed $EMOTES on $TARGET_ID"
+
+#TARGET_ID='6802639-24d573f4dfa1d7fd33-KAN-KANL-0000000000006503'
+#EMOTES='😠 😡 🎃 🧠 🍷'
+#emote && echo "Completed $EMOTES on $TARGET_ID"
